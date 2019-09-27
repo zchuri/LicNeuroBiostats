a2d0dea9f89db7edad09421b5921d2760a28aad5
zchuri <zgtabuenca@comunidad.unam.mx>
2019-09-26 20:07:30 -0500

    Descritive statistics updated!

--- 02_Descrip.R
+++ 02_Descrip.R
@@ -3,8 +3,13 @@
 # Clear workspace
 rm(list = ls())
 
+# Save original parameters
+dev.off()
+op <- par()
+
 # Load data
 help("datasets")
+help("quakes")
 
 # Frequencies
 DF <- datasets::quakes
@@ -12,16 +17,27 @@ View(DF)
 table(DF$stations)
 
 # Barplot
-barplot(table(DF$stations))
+barplot(table(DF$stations),
+        ylim = c(0,50),
+        col = "red")
 
 # Histogram
-hist(DF$mag)
+hist(DF$mag,
+     main = "Histograma de magnitudes",
+     xlab = "Magnitud")
 
 # Boxplot
-boxplot(DF$mag)
+boxplot(DF$mag, notch = T)
+# Create categoric variable
+DF$grupo <- DF$depth > median(DF$depth)
+DF$grupo <- factor(DF$grupo)
+levels(DF$grupo) <- c("No-deep","Deep")
+# Grouped notched-boxplot
+boxplot(DF$mag ~ DF$grupo, notch = T)
 
 # Scatterplot
 plot(DF$mag)
+plot(DF$mag[DF$mag>5])
 
 # Timeseries
 plot(DF$mag, type = "l")
@@ -35,13 +51,22 @@ sample()
 
 # Central tendency measures
 summary(DF$mag)
+fivenum(DF$mag)
 mean(DF$mag)
 median(DF$mag)
 mode(DF$mag)
 
 # Load package 'psych'
+install.packages("psych")
 library(psych)
 describe(DF$mag)
 
 # Quantiles
 quantile(DF$mag, 0.25)
+
+# Clear workspace
+rm(list = ls())
+datos <- read.csv("~/Documents/alturas.csv")
+getwd()
+summary(datos$Altura)
+boxplot(datos$Altura)
